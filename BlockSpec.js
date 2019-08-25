dbd3042fbb43aeb18b9d80ad5f435befea926ac4
Costa <costa@acer.com>
2019-08-25 19:59:59 +0300

    Improved formet of specs

--- BlockSpec.js
+++ BlockSpec.js
@@ -1,111 +1,113 @@
-describe('Check default rotation', ()=>{
-  it('should be 0', ()=>{
-    let block = new Block();
-    
-    expect(block.rotation).toBe(0);
-  })
-})
-
-describe('Check rotation', ()=>{
-  it('should rotate once ', ()=>{
-    let block = new Block();
-
-    block.rotateRight();
-    expect(block.rotation).toBe(1);
+describe('Block test suite', function () {
+  describe('Check default rotation', ()=>{
+    it('should be 0', ()=>{
+      let block = new Block();
+      
+      expect(block.rotation).toBe(0);
+    })
   })
 
-  it('should right rotate 360', ()=>{
-    let block = new Block();
+  describe('Check rotation', ()=>{
+    it('should rotate once ', ()=>{
+      let block = new Block();
 
-    for(let i=0; i<4; i++){
       block.rotateRight();
-    }
-    
-    expect(block.rotation).toBe(0);
+      expect(block.rotation).toBe(1);
+    })
+
+    it('should right rotate 360', ()=>{
+      let block = new Block();
+
+      for(let i=0; i<4; i++){
+        block.rotateRight();
+      }
+      
+      expect(block.rotation).toBe(0);
+    })
   })
-})
 
-describe('Check I piece rotation', ()=>{
-  it('should rotate piece to the right', ()=>{
-    let block = new BlockI();
+  describe('Check I piece rotation', ()=>{
+    it('should rotate piece to the right', ()=>{
+      let block = new BlockI();
 
-    expect(block.piece[0]).toEqual([1, 0, 0, 0]);
-    expect(block.piece[1]).toEqual([1, 0, 0, 0]);
-    expect(block.piece[2]).toEqual([1, 0, 0, 0]);
-    expect(block.piece[3]).toEqual([1, 0, 0, 0]);
+      expect(block.piece[0]).toEqual([1, 0, 0, 0]);
+      expect(block.piece[1]).toEqual([1, 0, 0, 0]);
+      expect(block.piece[2]).toEqual([1, 0, 0, 0]);
+      expect(block.piece[3]).toEqual([1, 0, 0, 0]);
 
-    block.rotatePiece();
+      block.rotatePiece();
 
-    expect(block.piece[0]).toEqual([1, 1, 1, 1]);
-    expect(block.piece[1]).toEqual([0, 0, 0, 0]);
-    expect(block.piece[2]).toEqual([0, 0, 0, 0]);
-    expect(block.piece[3]).toEqual([0, 0, 0, 0]);
+      expect(block.piece[0]).toEqual([1, 1, 1, 1]);
+      expect(block.piece[1]).toEqual([0, 0, 0, 0]);
+      expect(block.piece[2]).toEqual([0, 0, 0, 0]);
+      expect(block.piece[3]).toEqual([0, 0, 0, 0]);
+    })
   })
-})
 
-describe('Check T piece rotation', ()=>{
-  it('should rotate piece to the right', ()=>{
-    let block = new BlockT();
+  describe('Check T piece rotation', ()=>{
+    it('should rotate piece to the right', ()=>{
+      let block = new BlockT();
 
-    expect(block.piece[0]).toEqual([1, 1, 1]);
-    expect(block.piece[1]).toEqual([0, 1, 0]);
-    expect(block.piece[2]).toEqual([0, 0, 0]);
+      expect(block.piece[0]).toEqual([1, 1, 1]);
+      expect(block.piece[1]).toEqual([0, 1, 0]);
+      expect(block.piece[2]).toEqual([0, 0, 0]);
 
-    block.rotatePiece();
-    block.rotatePiece();
-    block.rotatePiece();
-    
-    expect(block.piece[0]).toEqual([1, 0, 0]);
-    expect(block.piece[1]).toEqual([1, 1, 0]);
-    expect(block.piece[2]).toEqual([1, 0, 0]);
+      block.rotatePiece();
+      block.rotatePiece();
+      block.rotatePiece();
+      
+      expect(block.piece[0]).toEqual([1, 0, 0]);
+      expect(block.piece[1]).toEqual([1, 1, 0]);
+      expect(block.piece[2]).toEqual([1, 0, 0]);
 
-    block.rotatePiece();
+      block.rotatePiece();
 
-    expect(block.piece[0]).toEqual([1, 1, 1]);
-    expect(block.piece[1]).toEqual([0, 1, 0]);
-    expect(block.piece[2]).toEqual([0, 0, 0]);
+      expect(block.piece[0]).toEqual([1, 1, 1]);
+      expect(block.piece[1]).toEqual([0, 1, 0]);
+      expect(block.piece[2]).toEqual([0, 0, 0]);
+    })
   })
-})
 
-describe('Check O piece rotation', ()=>{
-  it('rotation should have no effect', ()=>{
-    let block = new BlockO();
+  describe('Check O piece rotation', ()=>{
+    it('should not have any effect', ()=>{
+      let block = new BlockO();
 
-    expect(block.piece[0]).toEqual([1, 1]);
-    expect(block.piece[1]).toEqual([1, 1]);
+      expect(block.piece[0]).toEqual([1, 1]);
+      expect(block.piece[1]).toEqual([1, 1]);
 
-    block.rotatePiece();
-    expect(block.piece[0]).toEqual([1, 1]);
-    expect(block.piece[1]).toEqual([1, 1]);
+      block.rotatePiece();
+      expect(block.piece[0]).toEqual([1, 1]);
+      expect(block.piece[1]).toEqual([1, 1]);
 
-    block.rotatePiece();
-    expect(block.piece[0]).toEqual([1, 1]);
-    expect(block.piece[1]).toEqual([1, 1]);
+      block.rotatePiece();
+      expect(block.piece[0]).toEqual([1, 1]);
+      expect(block.piece[1]).toEqual([1, 1]);
+    })
   })
-})
 
-describe('Piece length should be reported', ()=>{
-  it('Check length of I piece', ()=>{
-    let block = new BlockI();
-    expect(block.length()).toEqual(4);
+  describe('Piece length should be reported', ()=>{
+    it('should report length of I piece', ()=>{
+      let block = new BlockI();
+      expect(block.length()).toEqual(4);
+    })
+    it('should report length of T piece', ()=>{
+      let block = new BlockT();
+      expect(block.length()).toEqual(2);
+      block.rotateRight();
+      expect(block.length()).toEqual(3);
+    })
   })
-  it('Check length of T piece', ()=>{
-    let block = new BlockT();
-    expect(block.length()).toEqual(2);
-    block.rotateRight();
-    expect(block.length()).toEqual(3);
-  })
-})
 
-describe('Piece width should be reported', ()=>{
-  it('Check width of I piece', ()=>{
-    let block = new BlockI();
-    expect(block.width()).toEqual(1);
-  })
-  it('Check width of T piece', ()=>{
-    let block = new BlockT();
-    expect(block.width()).toEqual(3);
-    block.rotateRight();
-    expect(block.width()).toEqual(2);
+  describe('Piece width should be reported', ()=>{
+    it('should report width of I piece', ()=>{
+      let block = new BlockI();
+      expect(block.width()).toEqual(1);
+    })
+    it('should report width of T piece', ()=>{
+      let block = new BlockT();
+      expect(block.width()).toEqual(3);
+      block.rotateRight();
+      expect(block.width()).toEqual(2);
+    })
   })
 })
--- PlayFieldSpec.js
+++ PlayFieldSpec.js
@@ -1,179 +1,173 @@
-describe('Default playfield after construction', ()=>{
-  it('should be all rows', ()=>{
-    let playfield = new PlayField();
-    
-    expect(playfield.field.length).toBe(20);
+describe('Playfield test suite', function () {
+
+  describe('Default playfield after construction', ()=>{
+    it('should have 20 rows', ()=>{
+      let playfield = new PlayField();
+      
+      expect(playfield.field.length).toBe(20);
+    })
+
+    it('should have 10 empty columns in each row', ()=>{
+      let playfield = new PlayField();
+      
+      for(let i=0; i<20; i++){
+        expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
+      }
+    })
   })
 
-  it('all rows should have 10 empty columns', ()=>{
-    let playfield = new PlayField();
-    
-    for(let i=0; i<20; i++){
-      expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
-    }
-  })
-})
+  describe('Clear playfield', ()=>{
+    it('should be 20 rows after being cleared', ()=>{
+      let playfield = new PlayField();
+      playfield.clearFields();
+      
+      expect(playfield.field.length).toBe(20);
+    })
 
-describe('Clear playfield', ()=>{
-  it('should be 20 rows', ()=>{
-    let playfield = new PlayField();
-    playfield.clearFields();
-    
-    expect(playfield.field.length).toBe(20);
-  })
-
-  it('all rows should have 10 empty columns', ()=>{
-    let playfield = new PlayField();
-    playfield.clearFields();
-    
-    for(let i=0; i<20; i++){
-      expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
-    }
-  })
-})
-
-describe('Random block generation', ()=>{
-  it('should return a valid block', ()=>{
-    let playfield = new PlayField();
-    let block = playfield.getRandomBlock();
-    
-    expect(block.piece.length >= 2).toBe(true);
+    it('should have 10 empty columns in each row after being cleared', ()=>{
+      let playfield = new PlayField();
+      playfield.clearFields();
+      
+      for(let i=0; i<20; i++){
+        expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
+      }
+    })
   })
 
-  it('all rows should have 10 empty columns', ()=>{
-    let playfield = new PlayField();
-    playfield.clearFields();
-    
-    for(let i=0; i<20; i++){
-      expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
-    }
-  })
-})
-
-describe('Get Field with Block', ()=>{
-  it('should return a valid field with a block', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    
-    expect(field[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
+  describe('Random block generation', ()=>{
+    it('should return a valid block', ()=>{
+      let playfield = new PlayField();
+      let block = playfield.getRandomBlock();
+      
+      expect(block.piece.length >= 2).toBe(true);
+    })
   })
 
-  it('main playfield should not include block', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    
-    for(let i=0; i<20; i++){
-      expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
-    }
-  })
-})
+  describe('Get Field with Block', ()=>{
+    it('should return a valid field with a block', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      
+      expect(field[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
+    })
 
-describe('Move block around the play field', ()=>{
-  it('block should be moved down', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    let firstRow = [];
-    for(let i=0; i<10; i++){
-      firstRow.push(field[0][i]);
-    }
-    playfield.moveBlockDown();
-    let field2 = playfield.getFieldWithBlock();
-
-    expect(field2[1]).toEqual(firstRow);
+    it('should return main playfield without block', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      
+      for(let i=0; i<20; i++){
+        expect(playfield.field[i]).toEqual([0,0,0,0,0,0,0,0,0,0]);
+      }
+    })
   })
 
-  it('block should be docked at the bottom of field when moved dow', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    let firstRow = [];
-    for(let i=0; i<10; i++){
-      firstRow.push(field[0][i]);
-    }
-    for(let i=0; i<56; i++){
+  describe('Move block around the play field', ()=>{
+    it('should move the block down', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
       playfield.moveBlockDown();
-    }
+      let field2 = playfield.getFieldWithBlock();
 
-    let field2 = playfield.getFieldWithBlock();
+      expect(field2[1]).toEqual(firstRow);
+    })
 
-    expect(field2[17]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
+    it('should docked the block at the bottom moved down many times', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
+      for(let i=0; i<56; i++){
+        playfield.moveBlockDown();
+      }
 
-    //console.log(playfield.fieldWithBlock);
-  })
+      let field2 = playfield.getFieldWithBlock();
 
-  it('block should be moved right', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    let firstRow = [];
-    for(let i=0; i<10; i++){
-      firstRow.push(field[0][i]);
-    }
-    playfield.moveBlockRight();
-    let field2 = playfield.getFieldWithBlock();
+      expect(field2[17]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
 
-    expect(field2[0][1]).toEqual(firstRow[0]);
-    expect(field2[0][2]).toEqual(firstRow[1]);
-    expect(field2[0][3]).toEqual(firstRow[2]);
-    expect(field2[0][4]).toEqual(firstRow[3]);
-    expect(field2[0][5]).toEqual(firstRow[4]);
-    expect(field2[0][6]).toEqual(firstRow[5]);
-    expect(field2[0][7]).toEqual(firstRow[6]);
-    expect(field2[0][8]).toEqual(firstRow[7]);
-    expect(field2[0][9]).toEqual(firstRow[8]);
-  })
+      //console.log(playfield.fieldWithBlock);
+    })
 
-  it('block should be moved to far right', ()=>{
-    let playfield = new PlayField();
-    let field = playfield.getFieldWithBlock();
-    let firstRow = [];
-    for(let i=0; i<10; i++){
-      firstRow.push(field[0][i]);
-    }
-    for(let i=0; i<56; i++){
+    it('should move the block to the right', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
       playfield.moveBlockRight();
-    }
-    let field2 = playfield.getFieldWithBlock();
-    //console.log(field2);
+      let field2 = playfield.getFieldWithBlock();
 
-    expect(field2[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
-    expect(field2[0].length).toEqual(10);
-  })
-})
+      expect(field2[0][1]).toEqual(firstRow[0]);
+      expect(field2[0][2]).toEqual(firstRow[1]);
+      expect(field2[0][3]).toEqual(firstRow[2]);
+      expect(field2[0][4]).toEqual(firstRow[3]);
+      expect(field2[0][5]).toEqual(firstRow[4]);
+      expect(field2[0][6]).toEqual(firstRow[5]);
+      expect(field2[0][7]).toEqual(firstRow[6]);
+      expect(field2[0][8]).toEqual(firstRow[7]);
+      expect(field2[0][9]).toEqual(firstRow[8]);
+    })
 
-it('block should be moved left', ()=>{
-  let playfield = new PlayField();
-  let field = playfield.getFieldWithBlock();
-  let firstRow = [];
-  for(let i=0; i<10; i++){
-    firstRow.push(field[0][i]);
-  }
-  playfield.moveBlockLeft();
-  let field2 = playfield.getFieldWithBlock();
+    it('should move the block to the far right', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
+      for(let i=0; i<56; i++){
+        playfield.moveBlockRight();
+      }
+      let field2 = playfield.getFieldWithBlock();
+      //console.log(field2);
 
-  expect(field2[0][0]).toEqual(firstRow[1]);
-  expect(field2[0][0]).toEqual(firstRow[2]);
-  expect(field2[0][2]).toEqual(firstRow[3]);
-  expect(field2[0][3]).toEqual(firstRow[4]);
-  expect(field2[0][4]).toEqual(firstRow[5]);
-  expect(field2[0][5]).toEqual(firstRow[6]);
-  expect(field2[0][6]).toEqual(firstRow[7]);
-  expect(field2[0][7]).toEqual(firstRow[8]);
-  expect(field2[0][8]).toEqual(firstRow[9]);
-})
-
-it('block should be moved to far left', ()=>{
-  let playfield = new PlayField();
-  let field = playfield.getFieldWithBlock();
-  let firstRow = [];
-  for(let i=0; i<10; i++){
-    firstRow.push(field[0][i]);
-  }
-  for(let i=0; i<56; i++){
-    playfield.moveBlockLeft();
-  }
+      expect(field2[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
+      expect(field2[0].length).toEqual(10);
+    })
   
-  let field2 = playfield.getFieldWithBlock();
-  //console.log(field2);
+    it('should move the block to the left', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
+      playfield.moveBlockLeft();
+      let field2 = playfield.getFieldWithBlock();
 
-  expect(field2[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
-  expect(field2[0].length).toEqual(10);
+      expect(field2[0][0]).toEqual(firstRow[1]);
+      expect(field2[0][0]).toEqual(firstRow[2]);
+      expect(field2[0][2]).toEqual(firstRow[3]);
+      expect(field2[0][3]).toEqual(firstRow[4]);
+      expect(field2[0][4]).toEqual(firstRow[5]);
+      expect(field2[0][5]).toEqual(firstRow[6]);
+      expect(field2[0][6]).toEqual(firstRow[7]);
+      expect(field2[0][7]).toEqual(firstRow[8]);
+      expect(field2[0][8]).toEqual(firstRow[9]);
+    })
+
+    it('should move the block to the far left', ()=>{
+      let playfield = new PlayField();
+      let field = playfield.getFieldWithBlock();
+      let firstRow = [];
+      for(let i=0; i<10; i++){
+        firstRow.push(field[0][i]);
+      }
+      for(let i=0; i<56; i++){
+        playfield.moveBlockLeft();
+      }
+      
+      let field2 = playfield.getFieldWithBlock();
+      //console.log(field2);
+
+      expect(field2[0]).not.toEqual([0,0,0,0,0,0,0,0,0,0]);
+      expect(field2[0].length).toEqual(10);
+    })
+  })
 })
