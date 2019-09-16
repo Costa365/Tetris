598efefb7e2f0ee00b75b7c7c3ecc9410e261ee0
Costa <costa@acer.com>
2019-09-16 23:00:35 +0300

    Tidied up TetrisGame

--- TetrisGame.js
+++ TetrisGame.js
@@ -1,18 +1,28 @@
+const CANVAS_WIDTH = 300;
+const CANVAS_HEIGHT = 600;
+const SCORE_X_POS = 7;
+const LINES_Y_POS = 17;
+const SCORE_Y_POS = 41;
+const LEVEL_Y_POS = 65;
+const NEXT_Y_POS = 89;
+const GAME_OVER_Y_POS = 89;
+const NEXT_PIECE_Y_OFFSET = 96;
+const SCORE_PANEL_WIDTH = 135;
+
 let animate = function (callback) { window.setTimeout(callback, 10)};
 	
 let canvas = document.createElement("canvas");
-let width = 300;
-let height = 600;
-let framespersec = 7; // 70 -> 7
+let width = CANVAS_WIDTH;
+let height = CANVAS_HEIGHT;
 let frame = 0;
-canvas.width = width+135;
-canvas.height = height;
 let context = canvas.getContext('2d');
 let playfield = new PlayField();
 let blockwidth = width/playfield.columns();
 let blockheight = height/playfield.rows();
-
 let keysDown = {};
+
+canvas.width = width+SCORE_PANEL_WIDTH;
+canvas.height = height;
 
 let render = function () {
   context.fillStyle = "#000080";
@@ -20,10 +30,10 @@ let render = function () {
   context.fillStyle = "#000050";
   context.fillRect(width + 5, 0, 130, height);
   context.fillStyle = "#ffffff";
-  context.fillText(`Lines: ${playfield.getLines()}`, width + 7, 10);
-  context.fillText(`Score: ${playfield.getScore()}`, width + 7, 33);
-  context.fillText(`Level: ${playfield.getLevel()}`, width + 7, 56);
-  context.fillText(`Next: `, width + 7, 79);
+  context.font = "20px Monospace";
+  context.fillText(`Lines: ${playfield.getLines()}`, width + SCORE_X_POS, LINES_Y_POS);
+  context.fillText(`Score: ${playfield.getScore()}`, width + SCORE_X_POS, SCORE_Y_POS);
+  context.fillText(`Level: ${playfield.getLevel()}`, width + SCORE_X_POS, LEVEL_Y_POS);
 
   context.fillStyle = "#ffff00";
   // Draw field with piece
@@ -44,11 +54,15 @@ let render = function () {
 };
 
 let drawNextPiece = function (){
+  context.fillStyle = "#ffffff";
+  context.fillText(`Next: `, width + SCORE_X_POS, NEXT_Y_POS);
+  context.fillStyle = "#ffff00";
   let piece = playfield.getNextPiece();
   for(let i = 0; i < piece.length; i++) {
     for(let j = 0; j < piece[0].length; j++) {
       if(piece[i][j]==1){
-        context.fillRect(j*blockwidth  + width + 7, i*blockheight+86, blockwidth, blockheight);
+        context.fillRect(j*blockwidth  + width + SCORE_X_POS, 
+          i*blockheight+NEXT_PIECE_Y_OFFSET, blockwidth, blockheight);
       }
     }
   }
@@ -56,7 +70,7 @@ let drawNextPiece = function (){
 
 let drawGameOver = function (){
   context.fillStyle = "#ffffff";
-  context.fillText(`Game Over!`, width + 7, 100);
+  context.fillText(`Game Over!`, width + SCORE_X_POS, GAME_OVER_Y_POS);
 }
 
 let update = function () {
