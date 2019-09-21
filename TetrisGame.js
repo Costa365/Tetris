b5e024b1543b254dd4f2eee0006be5ce78e7565a
Costa <costa@acer.com>
2019-09-21 21:24:32 +0300

    Added border and improved style

--- TetrisGame.js
+++ TetrisGame.js
@@ -1,6 +1,6 @@
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 600;
-const SCORE_X_POS = 7;
+const SCORE_X_POS = 4;
 const LINES_Y_POS = 17;
 const SCORE_Y_POS = 41;
 const LEVEL_Y_POS = 65;
@@ -8,6 +8,9 @@ const NEXT_Y_POS = 89;
 const GAME_OVER_Y_POS = 89;
 const NEXT_PIECE_Y_OFFSET = 96;
 const SCORE_PANEL_WIDTH = 135;
+const SCORE_BACKGROUND_COLOR = "#404040";
+const FIELD_BACKGROUND_COLOR = "#ffffff";
+const SCORE_TEXT_COLOR = "#ffffff";
 
 let animate = function (callback) { window.setTimeout(callback, 10)};
 	
@@ -23,19 +26,19 @@ let keysDown = {};
 
 canvas.width = width+SCORE_PANEL_WIDTH;
 canvas.height = height;
+canvas.style = "border:1px solid " + SCORE_BACKGROUND_COLOR;
 
 let render = function () {
-  context.fillStyle = "#ffffff";
+  context.fillStyle = FIELD_BACKGROUND_COLOR;
   context.fillRect(0, 0, width, height);
-  context.fillStyle = "#606060";
-  context.fillRect(width + 5, 0, 130, height);
-  context.fillStyle = "#ffffff";
+  context.fillStyle = SCORE_BACKGROUND_COLOR;
+  context.fillRect(width + 2, 0, SCORE_PANEL_WIDTH, height);
+  context.fillStyle = SCORE_TEXT_COLOR;
   context.font = "20px Monospace";
   context.fillText(`Lines: ${playfield.getLines()}`, width + SCORE_X_POS, LINES_Y_POS);
   context.fillText(`Score: ${playfield.getScore()}`, width + SCORE_X_POS, SCORE_Y_POS);
   context.fillText(`Level: ${playfield.getLevel()}`, width + SCORE_X_POS, LEVEL_Y_POS);
 
-  context.fillStyle = "#ffff00";
   // Draw field with piece
   let field = playfield.getFieldWithPiece();
   for(let i = 0; i < field.length; i++) {
@@ -55,9 +58,8 @@ let render = function () {
 };
 
 let drawNextPiece = function (){
-  context.fillStyle = "#ffffff";
+  context.fillStyle = SCORE_TEXT_COLOR;
   context.fillText(`Next: `, width + SCORE_X_POS, NEXT_Y_POS);
-  context.fillStyle = "#ffff00";
   let piece = playfield.getNextPiece();
   for(let i = 0; i < piece.length; i++) {
     for(let j = 0; j < piece[0].length; j++) {
@@ -70,7 +72,7 @@ let drawNextPiece = function (){
 }
 
 let drawGameOver = function (){
-  context.fillStyle = "#ffffff";
+  context.fillStyle = SCORE_TEXT_COLOR;
   context.fillText(`Game Over!`, width + SCORE_X_POS, GAME_OVER_Y_POS);
 }
 
@@ -109,7 +111,6 @@ let step = function () {
     animate(step);
   }
 };
-
 
 document.body.appendChild(canvas);
 animate(step);
